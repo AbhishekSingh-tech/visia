13e6069d02a8972b2912afd681c72cf47f822845
aksingh <abhisheksingh@myn.global>
2024-10-15 21:22:57 +0530

    Reduced worker count and thread count to 1

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,8 +1,7 @@
 import os
-import multiprocessing
 
-workers = int(os.environ.get('GUNICORN_PROCESSES', multiprocessing.cpu_count()))
-threads = int(os.environ.get('GUNICORN_THREADS', '7'))
+workers = int(os.environ.get('GUNICORN_PROCESSES', '1'))
+threads = int(os.environ.get('GUNICORN_THREADS', '1'))
 timeout = int(os.environ.get('GUNICORN_TIMEOUT', '300'))
 bind = os.environ.get('GUNICORN_BIND', '0.0.0.0:8080')
 
